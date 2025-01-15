bc701b890b6015bd4a3d5de55ec4f203e90e7445
sayildiz <y.samet97@gmail.com>
2025-01-16 00:36:15 +0100

    upgrade sdk to 34->35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.sayildiz.spendingtrack"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
